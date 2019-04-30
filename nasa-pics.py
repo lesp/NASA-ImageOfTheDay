529ae61805195bdb41690662014f7933c3ec5bc0
Les Pounder <lespounder@gmail.com>
2019-04-30 11:28:56 +0100

    Updated with notification for Ubuntu

--- nasa-pics.py
+++ nasa-pics.py
@@ -1,13 +1,18 @@
 #! /usr/bin/env python3
 import requests
-url = "https://api.nasa.gov/planetary/apod?api_key=PUT YOUR API KEY HERE"
+import notify2
+notify2.init("NASA Astronomy Picture of the Day")
+url = "https://api.nasa.gov/planetary/apod?api_key=YOUR API KEY HERE"
 r = requests.get(url)
 if r:
     APOD = r.json()['url']
     pic = requests.get(APOD, allow_redirects=True)
     if  "jpg" not in APOD:
-        print("No image for today")
+        n = notify2.Notification("No image for today, must be a YouTube video")
+        n.show()
     else:
-		open('/home/les/Pictures/APOD.jpg', 'wb').write(pic.content)
+        open('/home/les/Pictures/APOD.jpg', 'wb').write(pic.content)
+        n = notify2.Notification("Enjoy todays image")
+        n.show()
 else:
     print("Error")
