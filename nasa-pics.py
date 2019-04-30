b56abca7a29e094aed832948a81b962837d6a171
Les Pounder <lespounder@gmail.com>
2019-04-30 10:23:26 +0100

    Updated with error handling

--- nasa-pics.py
+++ nasa-pics.py
@@ -1,9 +1,13 @@
+#! /usr/bin/env python3
 import requests
 url = "https://api.nasa.gov/planetary/apod?api_key=PUT YOUR API KEY HERE"
 r = requests.get(url)
 if r:
     APOD = r.json()['url']
     pic = requests.get(APOD, allow_redirects=True)
-    open('/home/les/Pictures/APOD.jpg', 'wb').write(pic.content)
+    if  "jpg" not in APOD:
+        print("No image for today")
+    else:
+		open('/home/les/Pictures/APOD.jpg', 'wb').write(pic.content)
 else:
     print("Error")
